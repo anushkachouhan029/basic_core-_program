aeec6d6a09d301ac5faaa5244b368cd89cbc6332
anushkachouhan029 <anushkachouhan029>
2023-01-08 23:15:16 +0530

    added even and odd project

---
+++ evenorodd/Program.cs
@@ -0,0 +1,31 @@
+﻿class VowelOrConsonant
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Enter any Alphabet: ");
+        char alphabet = Convert.ToChar(Console.ReadLine().ToLower());
+
+        switch (alphabet)
+        {
+            case 'a':
+                Console.WriteLine("Alphabet is Vowel");
+                break;
+            case 'e':
+                Console.WriteLine("Alphabet is Vowel");
+                break;
+            case 'i':
+                Console.WriteLine("Alphabet is Vowel");
+                break;
+            case 'o':
+                Console.WriteLine("Alphabet is Vowel");
+                break;
+            case 'u':
+                Console.WriteLine("Alphabet is Vowel");
+                break;
+            default:
+                Console.WriteLine("Alphabet is Consonant");
+                break;
+        }
+        Console.ReadLine();
+    }
+}
